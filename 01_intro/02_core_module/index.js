4ee4983bc6193c24d1cf6c94a383a238aff85148
pedroh-leite <pedrohleite.alves@gmail.com>
2023-10-06 15:00:32 -0300

    Starting module 2

---
+++ 01_intro/02_core_module/index.js
@@ -0,0 +1,11 @@
+const fs = require("fs"); //file system
+
+fs.readFile("arquivo.txt", "utf-8", (error, data) => {
+
+    if(error) {
+        console.log(error);
+    }
+
+    console.log(data);
+
+})
---
+++ 02_fundamentos/01_modulo_interno/index.js
@@ -0,0 +1,7 @@
+const modulo = require("./modulo");
+const soma = modulo.soma;
+
+soma(2, 3);
+soma(5, 10);
+
+modulo.soma(10, 10);
